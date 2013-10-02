29e460167ece2a9f7317bd6b6aeb8e855c48e576
Frederik Schumacher <frederik.schumacher@gmail.com>
2013-10-02 09:22:49 +0200

    frontend update

--- static/scripts/application.js
+++ static/scripts/application.js
@@ -1,7 +1,7 @@
-
+"use strict";
 
 $(function() {
-    // definitions
+    // models
     var LogRecord = Backbone.Model.extend({
         defaults: {
             visible: true
@@ -17,27 +17,44 @@ $(function() {
             logRecordStore.each(function(element){
                 if(element.get('name') == this.get('name')){
                     element.set({"visible": visible});
-                    console.log(element, element.get("visible"));
                 }
             }, this);
+        },
+        initialize: function() {
+            this.listenTo(logRecordStore, "add", function(logRecord) {
+                if (logRecord.get("name") == this.get("name"))
+                    this.set({"numLogRecords": this.get("numLogRecords") + 1});
+            });
         }
     });
+
+    // views
     var LogRecordView = Backbone.View.extend({
-        template: _.template($("#log-record-tpl").html()),
+        className: "row log-record",
+        template: _.template($("#log-record-tpl").html(), false, {"variable": "data"}),
         render: function(){
             this.$el.html(this.template(this.model.toJSON()));
             this.$el.toggle(this.model.get("visible"));
-            this.$el.find('time').timeago();
+            this.$el.find(".log-record-body").hide();
+            var levelnameClass = {
+                INFO: "alert-info",
+                WARNING: "alert-warning",
+                ERROR:"alert-danger",
+                CRITICAL: "alert-danger",
+                FATAL: "alert-danger"
+            }[this.model.get("levelname")];
+            if (levelnameClass)
+                this.$el.find(".badge").addClass(levelnameClass);
             return this;
         },
         initialize: function(){
            this.listenTo(this.model, 'change:visible', function(){
-               this.$el.slideToggle();
+               this.$el.fadeToggle("fast");
            }, this);
         }
     });
     var LogChannelView = Backbone.View.extend({
-        template: _.template($("#log-channel-tpl").html()),
+        template: _.template($("#log-channel-tpl").html(), false, {"variable": "data"}),
         tagName: 'a',
         className: "list-group-item",
         initialize: function(){
@@ -54,18 +71,42 @@ $(function() {
         },
         toggleVisibility: function(){
             var visible = this.model.get('visible');
-            this.$el.find('.glyphicon').toggleClass('glyphicon-ok-sign').toggleClass('glyphicon-remove-sign');
+            this.$el.find('.glyphicon')
+                .toggleClass('glyphicon-ok-sign')
+                .toggleClass('glyphicon-remove-sign');
             this.model.setVisible(!visible);
         }
     });
+    var ConnectionView = Backbone.View.extend({
+        el: $("#connection"),
+        setStatus: function(status) {
+            var $status_badge = this.$el.find("span.badge");
+            $status_badge.text(status);
+        }
+    });
+
+    // collections
     var LogRecordCollection = Backbone.Collection.extend({model: LogRecord});
     var LogChannelCollection = Backbone.Collection.extend({model: LogChannel});
 
     // instances
+    var connectionView = new ConnectionView;
     var logRecordStore = new LogRecordCollection;
     var logChannelStore = new LogChannelCollection;
 
     var service = new Service('ws://localhost:8888/logrecordsocket');
+    $(service).on("socket-connect", function() {
+        console.info("socket-connect");
+        connectionView.setStatus('connected');
+    });
+    $(service).on("socket-close", function() {
+        console.info("socket-close");
+        connectionView.setStatus('closed');
+    });
+    $(service).on("socket-error", function() {
+        console.info("socket-error");
+        connectionView.setStatus('error');
+    });
 
 
     logRecordStore.on('add', function(logRecord, collection, event){
@@ -80,15 +121,10 @@ $(function() {
 
     // create logrecord view and add to document
     $(service).on('logrecord', function(evt, record) {
-        console.info("service.logrecord", record);
         var logChannel = logChannelStore.findWhere({name: record.name});
         if (!logChannel) {
             logChannel = new LogChannel({"name": record.name});
             logChannelStore.add(logChannel);
-            logChannel.listenTo(logRecordStore, "add", function(logRecord) {
-                if (logRecord.get("name") == this.get("name"))
-                    this.set({"numLogRecords": this.get("numLogRecords") + 1});
-            });
         }
 
         var logRecord = new LogRecord(record);
@@ -98,8 +134,11 @@ $(function() {
 
     service.connect();
 
-    $("body").on("click", ".logrecord .panel-heading", function() {
-        $(this).next().slideToggle();
+    $("body").on("click", ".log-record .log-record-head", function() {
+        $(this).next().slideToggle("fast");
+        $(this).find("span.glyphicon")
+            .toggleClass("glyphicon-chevron-right")
+            .toggleClass("glyphicon-chevron-down")
     });
 
 });
