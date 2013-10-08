3bf71a0c18913f7b7721b65c09c48cc2c488f2a7
Frederik Schumacher <frederik.schumacher@gmail.com>
2013-10-08 08:06:33 +0200

    move tools to bin folder

--- bin/server.py
+++ bin/server.py
@@ -4,27 +4,31 @@ import logging.config
 from handler import LogRecordSocket
 import config
 from lib.amqp import TornadoClient
-import tornado.ioloop
-import tornado.web
-from tornado.web import URLSpec
+from tornado.web import (
+    URLSpec,
+    Application,
+    RedirectHandler,
+    StaticFileHandler,
+)
+from tornado.ioloop import IOLoop
 
 
 logging.config.dictConfig(config.log)
 logger = logging.getLogger('application')
 handlers = [
     URLSpec(r"/logrecordsocket", LogRecordSocket, name="logrecordsocket"),
-    URLSpec(r"/", tornado.web.RedirectHandler, {"url": "/index.html"}),
-    URLSpec(r"/(.*)", tornado.web.StaticFileHandler, dict(
+    URLSpec(r"/", RedirectHandler, {"url": "/index.html"}),
+    URLSpec(r"/(.*)", StaticFileHandler, dict(
         path=config.tornado.pop('static_path')
     ), name='static'),
 ]
-application = tornado.web.Application(handlers, **config.tornado)
+application = Application(handlers, **config.tornado)
 application.amqp_client = TornadoClient(config.tornado["amqp_client"]["url"])
 
 
 if __name__ == "__main__":
     application.listen(8888, address='0.0.0.0')
-    ioloop = tornado.ioloop.IOLoop.instance()
+    ioloop = IOLoop.instance()
     ioloop.add_timeout(500, application.amqp_client.connect)
     try:
         ioloop.start()
